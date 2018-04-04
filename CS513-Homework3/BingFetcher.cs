11e3752fad849a44dadd99a4d79246f0c2dbf01f
mew1876 <mew1876@gmail.com>
2018-04-04 15:19:55 -0500

    Added sync and async getTile functions

--- CS513-Homework3/BingFetcher.cs
+++ CS513-Homework3/BingFetcher.cs
@@ -1,14 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Drawing;
+using System.Net.Http;
+using System.IO;
 
 namespace CS513_Homework3
 {
     class BingFetcher
     {
-        Bitmap x;
+        static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            Image image = getTile("http://h0.ortho.tiles.virtualearth.net/tiles/a023131022213211200.jpeg?g=131");
+            image.Save("tileImage.jpg");
+        }
+
+        static Image getTile(string URI)
+        {
+            Image image = null;
+            Task get = getTileAsync(URI).ContinueWith(x => image = x.Result);
+            get.Wait();
+            return image;
+        }
+
+        static async Task<Image> getTileAsync(string URI)
+        {
+            Image image = null;
+            HttpResponseMessage response = await client.GetAsync(URI);
+            if(response.IsSuccessStatusCode)
+            {
+                byte[] bytes = await response.Content.ReadAsByteArrayAsync();
+                MemoryStream stream = new MemoryStream(bytes);
+                image = Image.FromStream(stream);
+            }
+            return image;
+        }
     }
 }
