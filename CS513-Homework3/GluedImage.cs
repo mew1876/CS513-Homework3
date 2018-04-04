3fbe34a4769de3067d9d34a182a9b29dbade7dd8
jujubochnak <jujubochnak@github.com>
2018-04-04 15:36:57 -0500

    empty Quilt Class

---
+++ CS513-Homework3/GluedImage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+/// <summary>
+/// Bitmap class that has methods which consist of "stitching"/placing images next to each other to make one big image
+/// </summary>
+public class GluedImage
+{
+	public Quilt()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+    private Bitmap GlueHorizontally(Bitmap leftImage, Bitmap rightImage)
+    {
+        Bitmap finalImage = new Bitmap(leftImage.Width + rightImage.Width, Math.Max(leftImage.Height, rightImage.Height));
+        using (Graphics g = Graphics.FromImage(finalImage))
+        {
+            g.DrawImage(leftImage, 0, 0);
+            g.DrawImage(rightImage, leftImage.Width, 0);
+        }
+    }
+}
